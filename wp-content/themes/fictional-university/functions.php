bb380c8f9ca2e179ef3fc339f18267affc5679cc
Dinesh Trivedi <dinesh.dart@gmail.com>
2020-03-24 20:24:11 +0200

    Section 5 - Item 14 - Page.php

--- wp-content/themes/fictional-university/functions.php
+++ wp-content/themes/fictional-university/functions.php
@@ -9,3 +9,10 @@ function univesity_files(){
 }
 
 add_action('wp_enqueue_scripts', 'univesity_files');
+
+
+function university_features() {
+    add_theme_support('title-tag');
+}
+
+add_action('after_setup_theme', 'university_features');
