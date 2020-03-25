7a29c3a25dc8ead72b694662a2ee75429e8da659
Dinesh Trivedi <dinesh.dart@gmail.com>
2020-03-25 20:56:41 +0200

    Section 5 - Menu of child page

--- wp-content/themes/fictional-university/page.php
+++ wp-content/themes/fictional-university/page.php
@@ -25,16 +25,33 @@
                 <?php }
             ?>
 
+            <?php
+                $childPagesOfCurrentPage = get_pages(array(
+                    'child_of' => get_the_ID(),
+                ));
 
-            <!--
+                if($parentPostId or $childPagesOfCurrentPage) { ?>
             <div class="page-links">
-                <h2 class="page-links__title"><a href="#">About Us</a></h2>
+                <h2 class="page-links__title"><a href="<?php echo get_permalink($parentPostId)?>"><?php echo get_the_title($parentPostId); ?></a></h2>
                 <ul class="min-list">
-                    <li class="current_page_item"><a href="#">Our History</a></li>
-                    <li><a href="#">Our Goals</a></li>
+
+                <?php
+                    if($parentPostId) {
+                        $findChildPostOf = $parentPostId;
+                    }
+                    else {
+                        $findChildPostOf = get_the_ID();
+                    }
+                    wp_list_pages(array(
+                        'title_li' => NULL,
+                        'child_of' => $findChildPostOf,
+                        'sort_column' => 'menu_order',
+                    ));
+                ?>
                 </ul>
             </div>
-    -->
+            <?php } ?>
+
 
             <div class="generic-content">
                 <?php the_content(); ?>
